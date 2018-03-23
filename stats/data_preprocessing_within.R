d82464b43e0abcaae82aed60cd9fa8d643689644
manuelbohn <bohn@stanford.edu>
2018-03-23 13:14:03 -0700

    updating control exp for replication

--- stats/data_preprocessing_within.R
+++ stats/data_preprocessing_within.R
@@ -17,12 +17,14 @@ setwd("~/Work/CueStrength/git/stats/")
 # select all files from individual workers
 #files <- dir("~/Work/CueStrength/cosub_within_s/anonymized-results/")
 files <- dir("~/Work/CueStrength/cosub_within_control/anonymized-results/")
+#files <- dir("~/Work/CueStrength/cosub_within_control/sandbox-results/")
 
 #combine files into one dataframe
 raw <- data.frame()
 for (f in files) {
 #  jf <- paste("~/Work/CueStrength/cosub_within_s/anonymized-results/",f,sep="")
   jf <- paste("~/Work/CueStrength/cosub_within_control/anonymized-results/",f,sep="")
+#  jf <- paste("~/Work/CueStrength/cosub_within_control/sandbox-results/",f,sep="")
   jd <- fromJSON(paste(readLines(jf), collapse=""))
   id <- data.frame(workerid = jd$WorkerId, 
                    data = jd$answers$data$data
@@ -31,9 +33,9 @@ for (f in files) {
 }
 
 # convert into short format, drop unnecessary columns, rename variables and sort by id
-inf.data= melt(setDT(raw), measure = patterns( "^data.experiment","^data.cond","^data.agent","^data.leftFruit","^data.rightFruit","^data.tablePositionCorr","^data.pick","^data.inf","^data.trial","^data.rt", "^data.correct"))
-names(inf.data) = c("id","alltrial","experiment","condition","agent","leftObject","rightObject","targetOnTable","pick","target","trial","rt","correct") 
-inf.data $pick= str_sub(inf.data $pick,66,str_length(inf.data $pick)-4)
+inf.data= melt(setDT(raw), measure = patterns( "^data.experiment","^data.cond","^data.control","^data.agent","^data.leftFruit","^data.rightFruit","^data.tablePositionCorr","^data.pick","^data.inf","^data.trial","^data.rt", "^data.correct"))
+names(inf.data) = c("id","alltrial","experiment","condition","control","agent","leftObject","rightObject","targetOnTable","pick","target","trial","rt","correct") 
+inf.data $pick= str_sub(inf.data $pick,60,str_length(inf.data $pick)-4)
 inf.data = inf.data[!duplicated(inf.data), ]
 inf.data = inf.data[order(id)]
 inf.data $id = paste(inf.data $id, inf.data $experiment,sep="_")
