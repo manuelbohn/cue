f146653d8c3c89e9263a81eb3b00b4c1705a266b
manuelbohn <bohn@stanford.edu>
2018-05-05 13:41:06 -0700

    unintentional control between

--- stats/data_preprocessing_within.R
+++ stats/data_preprocessing_within.R
@@ -15,22 +15,13 @@ setwd("~/Work/CueStrength/git/stats/")
 
 
 # select all files from individual workers
-#files <- dir("~/Work/CueStrength/cosub_within_s/anonymized-results/")
-#files <- dir("~/Work/CueStrength/cosub_within_control/anonymized-results/")
-#files <- dir("~/Work/CueStrength/cosub_within_control2/anonymized-results/")
-#files <- dir("~/Work/CueStrength/cosub_between_control_barrier/anonymized-results/")
-files <- dir("~/Work/CueStrength/cosub_between_control_later/anonymized-results/")
-#files <- dir("~/Work/CueStrength/cosub_between_control_test/anonymized-results/")
+
+files <- dir("~/Work/CueStrength/nosub_unintentional_tornado/production-results/")
 
 #combine files into one dataframe
 raw <- data.frame()
 for (f in files) {
-#  jf <- paste("~/Work/CueStrength/cosub_within_s/anonymized-results/",f,sep="")
-#  jf <- paste("~/Work/CueStrength/cosub_within_control/anonymized-results/",f,sep="")
-#  jf <- paste("~/Work/CueStrength/cosub_within_control2/anonymized-results/",f,sep="")
-  jf <- paste("~/Work/CueStrength/cosub_between_control_later/anonymized-results/",f,sep="")
-#  jf <- paste("~/Work/CueStrength/cosub_between_control_barrier/anonymized-results/",f,sep="")
-#  jf <- paste("~/Work/CueStrength/cosub_between_control_test/anonymized-results/",f,sep="")
+  jf <- paste("~/Work/CueStrength/nosub_unintentional_tornado/production-results/",f,sep="")
   
   jd <- fromJSON(paste(readLines(jf), collapse=""))
   id <- data.frame(workerid = jd$WorkerId, 
@@ -45,17 +36,20 @@ names(inf.data) = c("id","alltrial","experiment","condition","control","agent","
 inf.data $pick= str_sub(inf.data $pick,60,str_length(inf.data $pick)-4)
 inf.data = inf.data[!duplicated(inf.data), ]
 inf.data = inf.data[order(id)]
-inf.data $id = paste(inf.data $id, inf.data $experiment,sep="_")
+#inf.data $id = paste(inf.data $id, inf.data $experiment,sep="_")
 inf.data$trial[inf.data$trial=="train1"]="train"
 inf.data$trial[inf.data$trial=="train2"]="train"
 
 # check resulting datafile
 str(inf.data)
 head(inf.data)
+
 # write csv file for further analysis
+
+write.csv(inf.data, file="cue_unintentional_tornado.data.csv")
 #write.csv(inf.data, file="cue.data.csv")
 #write.csv(inf.data, file="cue_control_2.data.csv")
-write.csv(inf.data, file="cue_control_later.data.csv")
+#write.csv(inf.data, file="cue_control_later.data.csv")
 #write.csv(inf.data, file="cue_control_barrier.data.csv")
 #write.csv(inf.data, file="cue_control_test.data.csv")
 
